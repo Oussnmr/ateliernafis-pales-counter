6a7c2cda252be638731a4671d0e57c6fb4d5d331
Oussnmr <Oussama.nemri@hotmail.com>
2024-11-08 23:56:20 +0100

    code 2

--- app.js
+++ app.js
@@ -2,13 +2,16 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
+// Middleware pour parser les requêtes JSON
+app.use(express.json());
+
+// Route pour la page d'accueil
 app.get('/', (req, res) => {
   res.send('Compteur Pales - Atelier Nafis');
 });
 
+// Laissez une place pour les routes API Shopify et les webhooks
+
 app.listen(port, () => {
   console.log(`Serveur démarré sur le port ${port}`);
-"kdgedvgeievgivgici c'est oaris mon lmlmvieux iervehrv"  
 });
-
-
